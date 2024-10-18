c43d300dd1386ca1625f2e6e2a1ed4b03b3fcccb
kaynanoliveira <kaynanoliveira357@gmail.com>
2024-10-18 01:38:36 -0300

    commit

---
+++ 05-npm-packages-basic/src/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import logSymbols from 'log-symbols'
+
+console.log(logSymbols.success, chalk.green.bgGreen("Sucesso!"))
+
+console.log(logSymbols.error, chalk.red("Atenção!"))
