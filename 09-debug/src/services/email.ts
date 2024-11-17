335fb56d631f29208b8423f3db0005a856bedad0
kayna <kaynanoliveira357@gmail.com>
2024-11-17 01:59:55 -0300

    commit

--- 09-debug/src/services/email.ts
+++
@@ -1,9 +0,0 @@
-async function getBaseEmail(senderName: string): Promise<String>{
-   let base = `Olá ${senderName}, gostaria de me inscrever para uma vaga`
-    
-    base += "\n estou deixando meu curriculo"
-
-    return base
-}
-
-export {getBaseEmail}
